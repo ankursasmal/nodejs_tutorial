7c791c3f2230faa24c963fe62ce502ddc98c1b7e
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-03-08 19:06:33 +0530

    complete

--- pathmodule.js
+++
@@ -1,4 +0,0 @@
-let path=require('path');
-console.log(path.basename("/Users/ankursasmal/node_js/pathmodule.js"))
-let x=(path.parse("/Users/ankursasmal/node_js/pathmodule.js"));
-console.log(x.name,x.root)
